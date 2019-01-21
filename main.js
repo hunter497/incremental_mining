eea9bcc818a7d24829633991444e5d45267fcca5
Nick Hunter <hunter.497@gmail.com>
2019-01-21 00:42:07 -0500

    Basic Prestige Section, kind of sloppy code. Unit tests needed.

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-// 'Levels' of the game, increase these every time the user prestiges. Precious metal mines that have higher value metals.
-var MINE_LEVELS_ENUM = {
-    coal: 1,
-    iron: 5,
-    gold: 50,
-    diamond: 500,
-    admantite: 10000,
-    space_rock: 1000000
-}
+// 'Levels' of the game, increase these every time the user prestiges. Precious metal mines that have higher value metals. Coal -> Iron -> Gold -> Diamond -> Adamantite -> Space Rock
+var MINE_TYPES = [
+    1,
+    5,
+    50,
+    500,
+    10000,
+    1000000
+]
 
-// Pickaxe upgrades that allow the user to mine a larger quantity of precious metals.
-var PICKAXE_UPGRADES_ENUM = {
-    basic: 1,
-    iron: 2,
-    adamantite: 4,
-    space_rock: 8
-}
+// Pickaxe upgrades that allow the user to mine a larger quantity of precious metals. Basic -> Iron -> Adamantite -> Space Rock
+var PICKAXE_UPGRADES = [
+    1,
+    2,
+    4,
+    8
+]
 
 var gameData = {}
 
@@ -28,9 +28,11 @@ var gameData = {}
             autoMinerLevel: 1,
             autoMinerCost: 50,
             cashPerSecond: 0,
-            pickaxeLevel: PICKAXE_UPGRADES_ENUM.basic,
-            mineLevel: MINE_LEVELS_ENUM.coal,
+            pickaxeLevel: PICKAXE_UPGRADES[0],
+            mineType: MINE_TYPES[0],
             prestigeCost: 1000,
+            prestigeLevel: 1,
+
         }
     };
     window.onload = setupGame();
@@ -51,7 +53,8 @@ var gameData = {}
 
 //TODO: split out these to another file, these will get unwieldy quickly as features are added.
 function mine() {
-    gameData.cash += gameData.cashPerClick * gameData.pickaxeLevel;
+    console.log(gameData.mineType);
+    gameData.cash += gameData.cashPerClick * gameData.pickaxeLevel * gameData.mineType;
     document.getElementById("cashMined").innerHTML = "$" + gameData.cash + " Mined";
 }
 
@@ -77,7 +80,7 @@ function buyAutoMiner() {
 }
 
 function generateCash() {
-    gameData.cash += gameData.cashPerSecond;
+    gameData.cash += gameData.cashPerSecond * gameData.mineType;
     document.getElementById("cashMined").innerHTML = "$" + gameData.cash + " Mined";
 }
 
@@ -87,17 +90,19 @@ function saveGame() {
 
 function prestige() {
     if(gameData.cash >= gameData.prestigeCost) {
-        gameData = {
-            cash: 1000,
-            cashPerClick: 1,
-            cashPerClickCost: 10,
-            autoMinerLevel: 1,
-            autoMinerCost: 50,
-            cashPerSecond: 0,
-            pickaxeLevel: PICKAXE_UPGRADES_ENUM.basic,
-            mineLevel: MINE_LEVELS_ENUM.iron,
-            prestigeCost: 1000,
-        }
-        console.log(gameData);
+        gameData.cash = 0;
+        gameData.cashPerClick = 1;
+        gameData.cashPerClickCost = 10;
+        gameData.autoMinerLevel = 1;
+        gameData.autoMinerCost = 50;
+        gameData.cashPerSecond = 0;
+        gameData.pickaxeLevel = PICKAXE_UPGRADES[0];
+        gameData.prestigeCost *= 10;
+        gameData.mineType = MINE_TYPES[gameData.prestigeLevel];
+        gameData.prestigeLevel = gameData.prestigeLevel + 1;
+        document.getElementById("cashMined").innerHTML = "$" + gameData.cash + " Mined";
+        document.getElementById("buyAutoMiner").innerHTML = "Buy auto miner (Currently Level " + gameData.autoMinerLevel + ") Cost: $" + gameData.autoMinerCost;
+        document.getElementById("perClickUpgrade").innerHTML = "Upgrade Pickaxe (Currently Level " + gameData.cashPerClick + ") Cost: $" + gameData.cashPerClickCost;
     }
+    
 }
